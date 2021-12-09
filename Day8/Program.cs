884c271b321cc538f38ee35bc3207e7ba8d49020
Rafał Kędziora <kontakt.rkedziora@gmail.com>
2021-12-09 23:33:10 +0100

    Day9

---
+++ Day8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+static string ComputeValue(List<string> inputsPattern, string line)
+{
+    if (line.Length == 5)
+        if (inputsPattern[0].All(x => line.Contains(x)))
+            return "3"; 
+        else if (inputsPattern[3].Where(x => !inputsPattern[2].Contains(x)).All(x => line.Contains(x)))
+            return "2";
+        else
+            return "5";
+    else if (line.Length == 6)
+        if (inputsPattern[1].All(x => line.Contains(x)))
+            return "0";
+        else if (inputsPattern[2].All(x => line.Contains(x)))
+            return "9";
+        else
+            return "6";
+    return null;
+}
+
+async Task<int> Day8(int project)
+{
+    List<string> fileContent = FileReader.FileToStringList(await FileReader.ReadFileAsync());
+    int result = 0;
+    int i = 1;
+    foreach (string line in fileContent)
+    {
+        List<string> singleLine = line.Split('|', StringSplitOptions.RemoveEmptyEntries).ToList();
+        List<string> outputs = singleLine[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+        if(project == 1)
+        result += outputs.Where(x => x.Length == 2 || x.Length == 3 || x.Length == 4 || x.Length == 7).Count();
+        else
+        {
+            List<string> inputsPattern = singleLine[0].Trim().Split(" ").Where(x => x.Length == 2 || x.Length == 3 || x.Length == 4 || x.Length == 7).OrderBy(i => i.Length).ToList();
+            StringBuilder sb = new StringBuilder();
+            foreach (var output in outputs)
+            {
+                sb.Append(output.Length == 2 ? "1" : output.Length == 3 ? "7" : output.Length == 4 ? "4" : output.Length == 7 ? "8" :
+                          ComputeValue(inputsPattern, output));
+            }
+            result += int.Parse(sb.ToString());
+        }
+    }
+    return result;
+}
+
+Console.WriteLine(await Day8(2));
---
+++ Day9/Program.cs
@@ -0,0 +1,71 @@
+﻿string fileContent = await FileReader.ReadFileAsync();
+List<string> content = FileReader.FileToStringList(fileContent);
+
+(int j, int i) neighbours(int j, int i, int[,] data)
+{
+    int min = int.MaxValue;
+    int minJ = j;
+    int minI = i;
+    if (j > 0 && data[j - 1, i] < min)
+        (min, minJ, minI) = (data[j - 1, i], j - 1, i);
+    if (j < content[i].Length - 1 && data[j+1, i] < min)
+        (min, minJ, minI) = (data[j + 1, i], j + 1, i);
+    if (i > 0 && data[j, i - 1] < min)
+        (min, minJ, minI) = (data[j, i-1], j, i-1);
+    if ((i < content.Count - 1) && data[j, i + 1] < min)
+        (min, minJ, minI) = (data[j, i + 1], j, i + 1);
+    return (minJ, minI);
+}
+
+int[,] Data()
+{
+    int[,] data = new int[content[0].Length, content.Count];
+    for (int i = 0; i < content.Count; i++)
+        for (int j = 0; j < content[i].Length; j++)
+            data[j, i] = (int)char.GetNumericValue(content[i][j]);
+    return data;
+}
+
+void Project1()
+{
+    int[,] data = Data();
+    int result = 0;
+
+    for (int i = 0; i < content.Count; i++)
+        for (int j = 0; j < content[i].Length; j++)
+        {
+            var neighbour = neighbours(j, i, data);
+            if (data[j, i] < data[neighbour.j, neighbour.i])
+                result += data[j, i] + 1;
+        }
+    Console.WriteLine(result);
+
+}
+
+void Project2()
+{
+    int[,] data = Data();
+    int[,] basinSizes = new int[content[0].Length, content.Count];
+    for(int i = 0; i < content.Count; i++)
+        for(int j=0; j < content[i].Length; j++)
+        {
+            int activeJ = j;
+            int activeI = i;
+            int current = data[j, i];
+            if (current == 9) 
+                continue;
+            (int flowJ, int flowI) = neighbours(j, i, data);
+            while (data[flowJ, flowI] < current)
+            {
+                activeJ = flowJ;
+                activeI = flowI;
+                current = data[flowJ, flowI];
+                (flowJ, flowI) = neighbours(activeJ, activeI, data);
+            }
+            basinSizes[activeJ, activeI]++;
+        }
+    Console.WriteLine(basinSizes.OfType<int>().OrderByDescending(x => x).Take(3).Aggregate(1, (x, y) => x * y));
+}
+
+Project1();
+Project2();
